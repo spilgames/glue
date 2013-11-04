061bfdc7d90804a2ceff65e9c9343c76f0426904
Jeroen Reurings <j.reurings@gmail.com>
2013-11-04 09:50:01 +0100

    - integration test

--- js/glue.js
+++ js/glue.js
@@ -6,33 +6,19 @@
  */
 var glue = (function (adapters) {
     'use strict';
-    return function () {
-        return {
-            audio: adapters.melonjs.audio,
-            event: adapters.melonjs.event,
-            input: adapters.melonjs.input,
-            levelManager: adapters.melonjs.levelManager,
-            loader: adapters.melonjs.loader,
-            module: adapters.spilgames.module,
-            plugin: adapters.melonjs.plugin,
-            state: adapters.melonjs.state,
-            sugar: adapters.spilgames.sugar,
-            video: adapters.melonjs.video
-        };
+    return {
+        audio: adapters.melonjs.audio,
+        event: adapters.melonjs.event,
+        input: adapters.melonjs.input,
+        levelManager: adapters.melonjs.levelManager,
+        loader: adapters.melonjs.loader,
+        module: adapters.spilgames.module,
+        plugin: adapters.melonjs.plugin,
+        state: adapters.melonjs.state,
+        sugar: adapters.spilgames.sugar,
+        video: adapters.melonjs.video
     };
 }(adapters));
-
-var glue = glue();
-
-glue.module.config({
-    paths: {
-        glue: 'modules'
-    }
-});
-
-glue.module.create('glue.core', function () {
-    console.log('hi...');
-});
 
 glue.game = glue.game || {};
 glue.game.namespace = 'game';
