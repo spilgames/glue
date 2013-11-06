b73e1c367c51a0f64f2610c3b22edb1f6f3ec1c9
Jeroen Reurings <j.reurings@gmail.com>
2013-11-06 15:53:25 +0100

    broken tests

--- test/spec/base/glue/api.js
+++ test/spec/base/glue/api.js
@@ -26,13 +26,67 @@
                 });
             });
             describe('Entities', function () {
-                it('Should be able to create an entity', function () {
+                it('Should be able to create an entity which gets updated in the game loop', function (done) {
                     glue.module.create('entity/player', function () {
-                        
+                        return function () {
+                            return me.ObjectEntity.extend({
+                                init: function (x, y, settings) {
+                                    this.parent(x, y, settings);
+                                    this.name = settings.name;
+                                },
+                                update: function () {
+
+                                }
+                            });
+                        };
+                    });
+                    glue.module.get(['entity/player'], function (Entity) {
+                        var testEntity = Entity();
+                        me.game.add(new testEntity(0, 0, {
+                            name: 'testPlayer',
+                            width: 10,
+                            height: 10
+                        }), 1);
+                        expect(me.game.getEntityByName('testPlayer')[0].name).toEqual('testPlayer');
+                        expect(1).toBe(0);
+                        done();
                     });
                 });
                 it('Should be able to add an entity to the game', function () {
-                    expect(0).toBe(1); 
+                    expect(0).toBe(1);
+                });
+
+                it('Should be able to spy on an event callback function', function (done) {
+                    glue.module.create('entity/player', function () {
+                        return function () {
+                            return me.ObjectEntity.extend({
+                                init: function (x, y, settings) {
+                                    this.parent(x, y, settings);
+                                    glue.event.on(glue.input.POINTER_MOVE, this.moveCallback);
+                                },
+                                update: function () {
+
+                                },
+                                moveCallback: function () {
+                                    console.log('called');
+                                }
+                            });
+                        };
+                    });
+                    glue.module.get(['entity/player'], function (Entity) {
+                        var testEntity = Entity();
+                        console.log(testEntity)
+                        var entity = new testEntity(0, 0, {
+                            name: 'testPlayer',
+                            width: 10,
+                            height: 10
+                        });
+                        console.log(entity);
+                        glue.event.fire(glue.input.POINTER_MOVE);
+                        spyOn(entity, 'moveCallback');
+                        expect(entity.moveCallback).toHaveBeenCalled();
+                        done();
+                    });
                 });
             });
         });
