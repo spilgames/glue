364cca6d28c382c4aa764346aa09eaffc6736c14
Jeroen Reurings <j.reurings@gmail.com>
2013-11-09 08:14:38 +0100

    - started draggable implementation

--- js/modules/spilgames/entity/behaviour/draggable.js
+++ js/modules/spilgames/entity/behaviour/draggable.js
@@ -11,82 +11,181 @@ glue.module.create(
     function (Glue) {
         /**
          * Constructor
-         * @memberOf draggable
+         * @name init
+         * @memberOf Draggable
          * @function
          * @param {Object} obj: the entity object
          */
         return function (obj) {
-            var isPressed = false,
+            // - per instance private members -
+            var dragging = false,
+                dragId = null,
+                grabOffset = new Vector(0, 0),
+                mouseDown = null,
+                mouseUp = null,
+                pointerId = null,
                 /**
-                 * Listens the POINTER_UP event
-                 * @name onPointerUp
-                 * @memberOf draggable
+                 * Gets called when the user starts dragging the entity
+                 * @name dragStart
+                 * @memberOf Draggable
                  * @function
-                 * @param {Object} evt: The pointer event
+                 * @param {Object} e: the pointer event
                  */
-                onPointerUp = function (evt) {
-                    isPressed = false;
+                dragStart = function (e) {
+                    if (dragging === false) {
+                        dragging = true;
+                        dragId = e.pointerId;
+                        grabOffset.set(e.gameX, e.gameY);
+                        grabOffset.sub(obj.pos);
+                        if (obj.dragStart) {
+                            obj.dragStart(e);
+                        }
+                        return false;
+                    }
                 },
                 /**
-                 * Listens the POINTER_DOWN event
-                 * @name onPointerDown
-                 * @memberOf draggable
+                 * Gets called when the user drags this entity around
+                 * @name dragMove
+                 * @memberOf Draggable
                  * @function
-                 * @param {Object} evt: The pointer event
+                 * @param {Object} e: the pointer event
                  */
-                onPointerDown = function (evt) {
-                    if(obj.isHovering()) {
-                        isPressed = true;
-                        // call the clicked method if it exists
-                        if (obj.clicked) {
-                            obj.clicked();
+                dragMove = function (e) {
+                    if (dragging === true) {
+                        if (dragId === e.pointerId) {
+                            obj.pos.set(e.gameX, e.gameY);
+                            obj.pos.sub(grabOffset);
+                            if (obj.dragMove) {
+                                obj.dragMove(e);
+                            }
                         }
                     }
                 },
                 /**
-                 * Sets up all events for this module
-                 * @name setupEvents
-                 * @memberOf draggable
+                 * Gets called when the user stops dragging the entity
+                 * @name dragEnd
+                 * @memberOf Draggable
                  * @function
+                 * @param {Object} e: the pointer event
                  */
-                setupEvents = function () {
-                    Glue.event.on(Glue.input.POINTER_DOWN, onPointerDown);
-                    Glue.event.on(Glue.input.POINTER_UP, onPointerUp);
+                dragEnd = function (e) {
+                    if (dragging === true) {
+                        pointerId = undefined;
+                        dragging = false;
+                        if (obj.dragEnd) {
+                            obj.dragEnd(e);
+                        }
+                        return false;
+                    }
                 },
                 /**
-                 * Tears down all events for this module
-                 * @name teardownEvents
-                 * @memberOf draggable
+                 * Translates a pointer event to a me.event
+                 * @name init
+                 * @memberOf Draggable
+                 * @function
+                 * @param {Object} e: the pointer event you want to translate
+                 * @param {String} translation: the me.event you want to translate
+                 * the event to
+                 */
+                translatePointerEvent = function (e, translation) {
+                    Event.publish(translation, [e, obj]);
+                },
+                /**
+                 * Initializes the events the modules needs to listen to
+                 * It translates the pointer events to me.events
+                 * in order to make them pass through the system and to make
+                 * this module testable. Then we subscribe this module to the
+                 * transformed events.
+                 * @name init
+                 * @memberOf Draggable
                  * @function
                  */
-                tearDownEvents = function () {
-                    Glue.event.off(Glue.input.POINTER_DOWN, onPointerDown);
-                    Glue.event.off(Glue.input.POINTER_UP, onPointerUp);
+                 initEvents = function () {
+                    mouseDown = function (e) {
+                        translatePointerEvent(e, Event.DRAGSTART);
+                    };
+                    mouseUp = function (e) {
+                        translatePointerEvent(e, Event.DRAGEND);
+                    };
+                    onPointerEvent('mousedown', obj, mouseDown);
+                    onPointerEvent('mouseup', obj, mouseUp);
+                    Event.subscribe(Event.MOUSEMOVE, dragMove);
+                    Event.subscribe(Event.DRAGSTART, function (e, draggable) {
+                        if (draggable === obj) {
+                            dragStart(e);
+                        }
+                    });
+                    Event.subscribe(Event.DRAGEND, function (e, draggable) {
+                        if (draggable === obj) {
+                            dragEnd(e);
+                        }
+                    });
                 };
 
-            // setup the module events
-            setupEvents();
-
-            return obj.mix({
+            // - external interface -
+            obj.mix({
                 /**
-                 * Returns if this entity is pressed
-                 * @name isPressed
-                 * @memberOf draggable
+                 * Destructor
+                 * @name destroy
+                 * @memberOf Draggable
                  * @function
                  */
-                isPressed: function () {
-                    return isPressed;
+                destroy: function () {
+                    Event.unsubscribe(Event.MOUSEMOVE, dragMove);
+                    Event.unsubscribe(Event.DRAGSTART, dragStart);
+                    Event.unsubscribe(Event.DRAGEND, dragEnd);
+                    removePointerEvent('mousedown', this);
+                    removePointerEvent('mouseup', this);
                 },
                 /**
-                 * Can be used to destruct this entity
-                 * @name isPressed
-                 * @memberOf draggable
+                 * Sets a callback function which will be called when this entity is dragged
+                 * @name setDragCallback
+                 * @memberOf Draggable
+                 * @function
+                 * @param {Function} callback: the callback function
+                 */
+                setDragCallback: function (callback) {
+                    dragCallback = callback;
+                },
+                /**
+                 * Sets a callback function which will be called when this entity is dropped
+                 * @name setDropCallback
+                 * @memberOf Draggable
+                 * @function
+                 * @param {Function} callback: the callback function
+                 */
+                setDropCallback: function (callback) {
+                    dropCallback = callback;
+                },
+                /**
+                 * Sets the grab offset of this entity
+                 * @name setGrabOffset
+                 * @memberOf Draggable
+                 * @function
+                 * @param {Number} x: the horitontal offset
+                 * @param {Number} y: the vertical offset
+                 */
+                setGrabOffset: function (x, y) {
+                    grabOffset = new Vector(x, y);
+                },
+                /**
+                 * Updates the entity per cycle, can be overwritten
+                 * @name update
+                 * @memberOf Draggable
                  * @function
                  */
-                destructClickable: function () {
-                    tearDownEvents();
+                update: function () {
+                    return true;
                 }
             });
+
+            // - initialisation logic -
+
+            // init drag related events
+            initEvents();
+            
+            // - return external interface -
+            return obj;
         };
     }
 );
