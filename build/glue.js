232eba7f07620fcc250012196de05e076838c791
Jeroen Reurings <j.reurings@gmail.com>
2013-12-20 10:03:26 +0100

    - deleted unused imports in animatable - created movable which can be used to set a target position and it will move the components visible to that position - integrated movable into Jailbreaker example game

--- build/glue.js
+++ build/glue.js
@@ -3100,14 +3100,7 @@ glue.module.create(
  */
 glue.module.create(
     'glue/component/animatable',
-    [
-        'glue',
-        'glue/component',
-        'glue/math/vector',
-        'glue/math/dimension',
-        'glue/math/rectangle'
-    ],
-    function (Glue, Component, Vector, Dimension, Rectangle) {
+    function () {
         return function (obj) {
             var animationSettings,
                 animations = {},
@@ -3505,6 +3498,88 @@ glue.module.create(
                 }
             };
             return obj;
+        };
+    }
+);
+
+/*
+ *  @module Movable
+ *  @namespace component
+ *  @desc Represents an movable component
+ *  @copyright (C) 2013 SpilGames
+ *  @author Jeroen Reurings
+ *  @license BSD 3-Clause License (see LICENSE file in project root)
+ */
+glue.module.create(
+    'glue/component/movable',
+    [
+        'glue',
+        'glue/math/vector'
+    ],
+    function (Glue, Vector) {
+        var Sugar = Glue.sugar;
+        return function (component) {
+            var position,
+                targetPosition = null,
+                moveSpeed = 100,
+                atTarget = true,
+                rotation = 0;
+
+            component = component || {};
+            component.movable = {
+                update: function (deltaT) {
+                    if (targetPosition !== null) {
+                        var radian,
+                            deltaX,
+                            deltaY;
+
+                        position = component.visible.getPosition();
+                        deltaX = targetPosition.x - position.x,
+                        deltaY = targetPosition.y - position.y;
+
+                        // Pythagorean theorem : c = √( a2 + b2 )
+                        // We stop moving if the remaining distance to the endpoint
+                        // is smaller then the step iterator (moveSpeed * deltaT).
+                        if (Math.sqrt(deltaX * deltaX + deltaY * deltaY) < moveSpeed * deltaT) {
+                            atTarget = true;
+                            position = targetPosition;
+                            component.visible.setPosition(position);
+                        } else {
+                            // Update the x and y position, using cos for x and sin for y
+                            // and get the right speed by multiplying by the speed and delta time.
+                            radian = Math.atan2(deltaY, deltaX);
+                            position.x += Math.cos(radian) * moveSpeed * deltaT;
+                            position.y += Math.sin(radian) * moveSpeed * deltaT;
+                            rotation = radian * 180 / Math.PI;
+                            component.visible.setPosition(position);                      
+                        }
+                    }
+                },
+                hasTarget: function () {
+                    return targetPosition !== null;
+                },
+                atTarget: function () {
+                    return atTarget;
+                },
+                getRotation: function () {
+                    return rotation;
+                },
+                setTarget: function (target) {
+                    if (!Sugar.isObject(target) && Sugar.isDefined(target.x) &&
+                        Sugar.isDefined(target.y)) {
+                            throw 'Invalid target supplied';
+                    }
+                    atTarget = false;
+                    targetPosition = target;
+                },
+                setMoveSpeed: function (speed) {
+                    if (!Sugar.isNumber(speed)) {
+                        throw 'Invalid speed supplied';
+                    }
+                    moveSpeed = speed;
+                }
+            };
+            return component;
         };
     }
 );
