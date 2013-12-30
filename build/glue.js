cf9fc289070da05125a9211f02bf4df621d8a99e
Jeroen Reurings <j.reurings@gmail.com>
2013-12-30 11:30:43 +0100

    - removed all old files from test - changed test init file to current Glue setup

--- build/glue.js
+++ build/glue.js
@@ -5763,6 +5763,10 @@ glue.module.create(
                             onReady();
                         }
                     });
+                } else {
+                    if (onReady) {
+                        onReady();
+                    }
                 }
             },
             shutdown: function () {
