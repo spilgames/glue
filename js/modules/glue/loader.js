d4ee3efc99fa79b2b14d1220330dcf4b8ec23442
Jeroen Reurings <j.reurings@gmail.com>
2014-02-19 10:34:46 +0100

    Added ASSET_TYPE_AUDIOSPRITE

--- js/modules/glue/loader.js
+++ js/modules/glue/loader.js
@@ -145,6 +145,9 @@ glue.module.create(
                     case module.ASSET_TYPE_BINARY:
                         loadBinary(name, source, assetLoadedHandler, assetErrorHandler);
                     break;
+                    case module.ASSET_TYPE_AUDIOSPRITE:
+                        loadAudioSprite(name, source, assetLoadedHandler, assetErrorHandler);
+                    break;
                 }
             },
             module = {
@@ -152,6 +155,7 @@ glue.module.create(
                 ASSET_TYPE_AUDIO: 'audio',
                 ASSET_TYPE_JSON: 'json',
                 ASSET_TYPE_BINARY: 'binary',
+                ASSET_TYPE_AUDIOSPRITE: 'audiosprite',
                 setAssetPath: function (value) {
                     assetPath = value;
                 },
