734d1e1c0be82c481c287a3c0b6c65fa74e2f66f
Jeroen Reurings <j.reurings@gmail.com>
2014-02-20 15:47:29 +0100

    refactored visible to spritable

---
+++ test/spec/modules/glue/component/spritable.js
@@ -0,0 +1,88 @@
+/**
+ *  @desc Spritable specs
+ *  @copyright (C) SpilGames
+ *  @author Jeroen Reurings
+ *  @license BSD 3-Clause License (see LICENSE file in project root)
+ */
+glue.module.create(
+    [
+        'glue/game',
+        'glue/math/vector',
+        'glue/math/dimension',
+        'glue/math/rectangle',
+        'glue/loader',
+        'glue/baseobject',
+        'glue/component/spritable'
+    ],
+    function (Game, Vector, Dimension, Rectangle, Loader, BaseObject, Spritable) {
+        describe('modules.glue.component.spritable', function () {
+            'use strict';
+
+            var object;
+
+            it('Should be able to create a valid spritable component', function (done) {
+                object = BaseObject(Spritable).add({
+                        init: function () {
+                            this.spritable.setup({
+                                position: Vector(100, 100),
+                                dimension: Dimension(200, 200),
+                                image: Loader.getAsset('glue'),
+                                origin: Vector(200, 200)
+                            });
+                        },
+                        draw: function (deltaT, context) {
+                            this.base.draw(deltaT, context);
+                        }
+                    });
+
+                spyOn(object, 'init').andCallThrough();
+                spyOn(object, 'draw').andCallThrough();
+
+                Game.add(object);
+
+                setTimeout(function () {
+                    // Check if the base object gets called by the Game in the proper way
+                    expect(object.init).toHaveBeenCalled();
+                    expect(object.draw).toHaveBeenCalledWith(
+                        jasmine.any(Number),
+                        jasmine.any(Object),
+                        jasmine.any(Object)
+                    );
+
+                    done();
+                }, 300);
+            });
+
+            it('Should be able to retrieve values which are set in the setup method', function () {
+                expect(object.getPosition().get()).toEqual(Vector(100, 100).get());
+                expect(object.getDimension().get()).toEqual(Dimension(200, 200).get());
+                expect(object.getOrigin().get()).toEqual(Vector(200, 200).get());
+                expect(object.getBoundingBox().get()).toEqual(Rectangle(100, 100, 300, 300).get());
+                expect(object.spritable.getImage()).toEqual(Loader.getAsset('glue'));
+            });
+
+            it('Should be able to set values by using setter methods', function () {
+                object.setPosition(Vector(200, 200));
+                object.setDimension(Dimension(400, 400));
+                object.setOrigin(Vector(300, 300));
+                object.setBoundingBox(Rectangle(10, 10, 100, 100));
+            });
+
+            it('Should be able to retrieve proper values which are set using setter methods', function () {
+                expect(object.getPosition().get()).toEqual(Vector(200, 200).get());
+                expect(object.getDimension().get()).toEqual(Dimension(400, 400).get());
+                expect(object.getOrigin().get()).toEqual(Vector(300, 300).get());
+                expect(object.getBoundingBox().get()).toEqual(Rectangle(10, 10, 100, 100).get());
+            });
+
+            it('Should update dimension and rectange when the image is updated', function () {
+                object.setOrigin(Vector(0, 0));
+                object.spritable.setImage(Loader.getAsset('spil'));
+                expect(object.spritable.getImage()).toEqual(Loader.getAsset('spil'));
+                expect(object.getDimension().get()).toEqual(Dimension(125, 92).get());
+                expect(object.getBoundingBox().get()).toEqual(Rectangle(200, 200, 325, 292).get());
+            });
+
+        });
+    }
+);
--- test/spec/modules/glue/sugar.js
+++ test/spec/modules/glue/sugar.js
@@ -136,7 +136,7 @@
             it('returns an object with properties from obj2 added to obj1 if the parameters are objects, overwriting property in obj1', function() {
                 var obj1 = {asd: 123, zxc: []};
                 var obj2 = {asd: 456, qwe: 'asd'};
-                var expected = {asd: 456, qwe: 'asd', zxc: []};
+                var expected = {asd: 456, zxc: [], base: {asd:123}, qwe:'asd'};
                 
                 expect(Sugar.combine(obj1, obj2)).toEqual(expected);
             });
@@ -285,95 +285,6 @@
                     domElement.className = 'one two three';
                     Sugar.toggleClass(domElement, 'four');
                     expect(domElement.className).toEqual('one two three four');
-                });
-            });
-            // prototype chain
-            describe('.mix', function () {
-                it('Should be able to mix basic modules', function () {
-                    var module1 = (function () {
-                        return {
-                            test1: function () {
-                                return 'test1';
-                            },
-                            test2: function () {
-                                return 'test2';
-                            }
-                        };
-                    }());
-                    var module2 = (function () {
-                        return {
-                            test3: function () {
-                                return 'test3';
-                            },
-                            test4: function () {
-                                return 'test4';
-                            }
-                        };
-                    }());
-                    var mixin = module1.mix(module2);
-                    expect(mixin.test1()).toEqual('test1');
-                    expect(mixin.test2()).toEqual('test2');
-                    expect(mixin.test3()).toEqual('test3');
-                    expect(mixin.test4()).toEqual('test4');
-                });
-                it('Should be able to mix in new abilities', function () {
-                    var base = (function () {
-                        return function () {
-                            return {
-                                base1: function () {
-                                    return 'base1';
-                                },
-                                base2: function () {
-                                    return 'base2';
-                                }
-                            }
-                        };
-                    }());
-                    var move = (function () {
-                        return function (obj) {
-                            obj.mix({
-                                run: function () {
-                                    return 'running';
-                                },
-                                walk: function () {
-                                    return 'walking';
-                                }
-                            });
-                        };
-                    }());
-                    var mixedEntity = (function (Base, Move) {
-                        return function (x, y, settings) {
-                            var postion = {
-                                    x: x,
-                                    y: y
-                                },
-                                base = Base(),
-                                move = base.mix(Move(base)),
-                                obj = base.mix({
-                                    custom1: function () {
-                                        return 'custom1';
-                                    },
-                                    custom2: function () {
-                                        return 'custom2';
-                                    },
-                                    getPosition: function () {
-                                        return postion;
-                                    }
-                                });
-                            return obj;
-                        };
-                    }(base, move));
-
-                    var entity = mixedEntity(200, 100);
-
-                    expect(entity.base1()).toEqual('base1');
-                    expect(entity.base2()).toEqual('base2');
-                    expect(entity.run()).toEqual('running');
-                    expect(entity.walk()).toEqual('walking');
-                    expect(entity.custom1()).toEqual('custom1');
-                    expect(entity.custom2()).toEqual('custom2');
-                    expect(entity.getPosition().x).toEqual(200);
-                    expect(entity.getPosition().y).toEqual(100);
                 });
             });
         });
