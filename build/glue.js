2c28a326ddca02b46f742515116ee2d71abe2c00
Marco Colombo <Marco.Colombo@spilgames.com>
2014-04-25 09:07:27 +0200

    Build updated

--- build/glue.js
+++ build/glue.js
@@ -8452,6 +8452,15 @@ glue.module.create(
                     return screens;
                 },
                 /**
+                 * Get a specific screen
+                 * @name getScreen
+                 * @memberOf Director
+                 * @function
+                 */
+                getScreen: function (value) {
+                    return screens[value];
+                },
+                /**
                  * Show a screen
                  * @name showScreen
                  * @memberOf Director
