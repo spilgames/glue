47cdaa1c0271f513a8c17f905d100e7c81ceec35
Marcolombo <marco.colombo@spilgames.com>
2014-05-08 09:40:54 +0200

    Update director.js
    Changed show to hide

--- js/modules/glue/director.js
+++ js/modules/glue/director.js
@@ -64,7 +64,7 @@ glue.module.create(
                     }
                     if (action === 'show') {
                         activeScreen = screen;
-                    } else if (action === 'show') {
+                    } else if (action === 'hide') {
                         activeScreen = null;
                     }
                 }
