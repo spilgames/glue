4101002131ef34465dc5643cacced4885ab11aba
Hernan Zhou <hernan.zhou@spilgames.com>
2014-05-08 09:43:37 +0200

    grunt

--- build/glue.js
+++ build/glue.js
@@ -8464,7 +8464,7 @@ glue.module.create(
                     }
                     if (action === 'show') {
                         activeScreen = screen;
-                    } else if (action === 'show') {
+                    } else if (action === 'hide') {
                         activeScreen = null;
                     }
                 }
